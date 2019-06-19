6c86a56f3275ab84add1d707a277c2612481ed32
Ajesh Vijayaragavan <32047538+AjeshV@users.noreply.github.com>
2019-06-19 05:11:56 -0400

    max-sqrt

---
+++ Codility/max-sqrt.java
@@ -0,0 +1,20 @@
+import java.lang.Math;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int A =  Integer.parseInt(args[0]), B = Integer.parseInt(args[1]);
+		int count = 0;
+		for (int i = A; i <= B; i++){
+			int current = 0, j = i;
+			if(Math.sqrt(j) == (int)Math.sqrt(j)) {
+				while(Math.sqrt(j) == (int)Math.sqrt(j)){
+					j = (int)Math.sqrt(j);
+					current++;
+				}
+				if (current > count) count = current;
+			}	
+	    }
+	    System.out.println(count);
+	}
+}
